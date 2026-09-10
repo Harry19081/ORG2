4e87c987a2c361aedba2f74b0820312f2de46c41
Harry He <20519290+Harry19081@users.noreply.github.com>
2026-09-10 20:02:20 +0530

    refactor(launchpad): remove unused launcher options and exports (#1537)

--- src/modules/shared/launchpad/components/index.ts
+++ src/modules/shared/launchpad/components/index.ts
@@ -1,7 +1,3 @@
-export { default as ContainerEnginesSection } from "./ContainerEnginesSection";
-export { default as ContainersSection } from "./ContainersSection";
-export { default as LaunchpadActionStrip } from "./LaunchpadActionStrip";
 export { default as LaunchpadDashboard } from "./LaunchpadDashboard";
-export { default as MacFolderIcon } from "./MacFolderIcon";
 
 export { RepoDetailPage } from "./RepoDetail";
