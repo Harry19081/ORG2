1a0c78e497ba0a98c1fe8998e3273bd255614bed
Harry19081 <20519290+Harry19081@users.noreply.github.com>
2026-09-11 14:46:00 +0530

    refactor(exports): drop barrel entries orphaned by the removals

--- src/modules/shared/layouts/blocks/index.ts
+++ src/modules/shared/layouts/blocks/index.ts
@@ -70,7 +70,7 @@ export {
   PANEL_HEADER_TOKENS,
   PanelRefreshButton,
 } from "./PanelHeader";
-export type { PanelHeaderBreadcrumb, PanelHeaderProps } from "./PanelHeader";
+export type { PanelHeaderProps } from "./PanelHeader";
 
 export { default as PanelFooter, PANEL_FOOTER_TOKENS } from "./PanelFooter";
 
